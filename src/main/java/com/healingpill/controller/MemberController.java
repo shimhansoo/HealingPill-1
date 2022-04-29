91bb4840711d6338ec49e75caddb63c9d68473a7
answhdgkr95 <49180647+answhdgkr95@users.noreply.github.com>
2022-04-29 11:39:17 +0900

    게시판(임시)

--- src/main/java/com/healingpill/controller/MemberController.java
+++ src/main/java/com/healingpill/controller/MemberController.java
@@ -1,3 +1,4 @@
+/*
 package com.healingpill.controller;
 
 import com.sun.org.slf4j.internal.Logger;
@@ -24,3 +25,4 @@ public class MemberController {
         logger.debug("로그인 페이지 이동");
     }
 }
+*/
